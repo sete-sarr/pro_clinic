ea15ae8f31592161767402fb9ceacf5c1abfcbcd
sete <sete@gmail.com>
2026-06-21 00:34:14 +0200

    modify the pipeline

--- comptes/api/urls.py
+++ comptes/api/urls.py
@@ -1,12 +1,11 @@
 from django.urls import path
 from .views import SendOptAPIView, VerifyOtpAPIView
-from rest_framework_simplejwt.views import (
-    TokenRefreshView,
-)
+from rest_framework_simplejwt.views import TokenRefreshView
+
 
 urlpatterns = [
     path('envoyer/otp/', SendOptAPIView.as_view(), name='send-otp'),   
     path('verify-otp/', VerifyOtpAPIView.as_view(), name='verify-otp'),
     path("token/refresh/", TokenRefreshView.as_view(), name="token_refresh",
-),
+)
 ]
